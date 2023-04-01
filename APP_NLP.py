6fdfa2682c249607347ad5450a38783c10c2c5a6
unknown <alisannan12@gmail.com>
2023-04-01 14:42:19 +0500

    Streamlit NLP APP

---
+++ APP_NLP.py
@@ -0,0 +1,31 @@
+import streamlit as st
+
+from app_nlp_fileupload import fileupload
+from app_nlp_processing import NLP
+
+PAGE_CONFIG = {
+    "page_title": "NLP Processing",
+    "page_icon": "random",
+    "layout": "wide",
+    "initial_sidebar_state": "expanded",
+}
+st.set_page_config(
+    **PAGE_CONFIG
+)
+
+
+def main():
+    menu = ["NLP", "Upload your File(NLP)", "About"]
+
+    choice = st.sidebar.selectbox("Menu", menu)
+
+    if choice == "NLP":
+        NLP()
+    elif choice == "Upload your File(NLP)":
+        fileupload()
+    else:
+        st.subheader("ABOUT")
+
+
+if __name__ == "__main__":
+    main()
